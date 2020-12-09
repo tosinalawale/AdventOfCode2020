b752e90917def952a2fc9359f83e6bdb0494ff04
tosinalawale <talawale@hotmail.com>
2020-12-09 20:11:57 +0000

    Day 4 solution

---
+++ AdventOfCode2020.Tests/DayFourTests.cs
@@ -0,0 +1,92 @@
+﻿namespace AdventOfCode2020.Tests
+{
+    using System.IO;
+    using FluentAssertions;
+    using NUnit.Framework;
+
+    public class DayFourTests
+    {
+        [Test]
+        public void Part1_CanExtractFirstPassportFromInput()
+        {
+            var input = new[]
+            {
+                "ecl:gry pid:860033327 eyr: 2020 hcl:#fffffd",
+                "byr: 1937 iyr: 2017 cid: 147 hgt: 183cm",
+                "",
+                "iyr:2013 ecl: amb cid:350 eyr: 2023 pid: 028048884",
+                "hcl:#cfa07d byr:1929",
+                "",
+                "hcl:#ae17e1 iyr:2013",
+                "eyr: 2024",
+                "ecl: brn pid:760753108 byr: 1931",
+                "hgt: 179cm",
+                "",
+                "hcl:#cfa07d eyr:2025 pid:166559648",
+                "iyr: 2011 ecl: brn hgt:59in"
+            };
+
+            DayFour.ExtractPassports(input)[0].Should().Be("ecl:gry pid:860033327 eyr:2020 hcl:#fffffd byr:1937 iyr:2017 cid:147 hgt:183cm");
+        }
+
+        [Test]
+        public void Part1_CanExtractAllPassportsFromInput()
+        {
+            var input = new[]
+            {
+                "ecl:gry pid:860033327 eyr: 2020 hcl:#fffffd",
+                "byr: 1937 iyr: 2017 cid: 147 hgt: 183cm",
+                "",
+                "iyr:2013 ecl: amb cid:350 eyr: 2023 pid: 028048884",
+                "hcl:#cfa07d byr:1929",
+                "",
+                "hcl:#ae17e1 iyr:2013",
+                "eyr: 2024",
+                "ecl: brn pid:760753108 byr: 1931",
+                "hgt: 179cm",
+                "",
+                "hcl:#cfa07d eyr:2025 pid:166559648",
+                "iyr: 2011 ecl: brn hgt:59in"
+            };
+
+            var expectedPassports = new[]
+            {
+                "ecl:gry pid:860033327 eyr:2020 hcl:#fffffd byr:1937 iyr:2017 cid:147 hgt:183cm",
+                "iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884 hcl:#cfa07d byr:1929",
+                "hcl:#ae17e1 iyr:2013 eyr:2024 ecl:brn pid:760753108 byr:1931 hgt:179cm",
+                "hcl:#cfa07d eyr:2025 pid:166559648 iyr:2011 ecl:brn hgt:59in"
+            };
+
+            DayFour.ExtractPassports(input).Should().BeEquivalentTo(expectedPassports);
+        }
+
+        [TestCase("ecl:gry pid:860033327 eyr:2020 hcl:#fffffd byr:1937 iyr:2017 cid:147 hgt:183cm", true)]
+        [TestCase("iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884 hcl:#cfa07d byr:1929", false)]
+        [TestCase("hcl:#ae17e1 iyr:2013 eyr:2024 ecl:brn pid:760753108 byr:1931 hgt:179cm", true)]
+        public void Part1_CanCheckPassportContainsAllRequiredFields(string passport, bool expectedIsValid)
+        {
+            DayFour.PassportContainsAllRequiredFields(passport).Should().Be(expectedIsValid);
+        }
+
+        [Test]
+        public void Part1_CalculateResultFromInput()
+        {
+            var input = File.ReadAllLines(@"Input/input4.txt");
+            System.Console.WriteLine(DayFour.CalculateResultForPartOne(input));
+        }
+
+        [TestCase("pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980 hcl:#623a2f", true)]
+        [TestCase("eyr:1972 cid:100 hcl:#18171d ecl:amb hgt:170 pid:186cm iyr:2018 byr:1926", false)]
+        public void Part2_CanCheckPassportContainsValidValuesForEachField(string passport, bool expectedIsValid)
+        {
+            DayFour.PassportContainsValidValuesForEachField(passport).Should().Be(expectedIsValid);
+        }
+
+        [Test]
+        public void Part2_CalculateResultFromInput()
+        {
+            var input = File.ReadAllLines(@"Input/input4.txt");
+            System.Console.WriteLine(DayFour.CalculateResultForPartTwo(input));
+        }
+    }
+}
---
+++ AdventOfCode2020/DayFour.cs
@@ -0,0 +1,127 @@
+﻿namespace AdventOfCode2020
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text.RegularExpressions;
+
+    public class DayFour
+    {
+        public static int CalculateResultForPartOne(string[] input)
+        {
+            var passports = ExtractPassports(input);
+
+            return passports.Count(PassportContainsAllRequiredFields);
+        }
+
+        public static int CalculateResultForPartTwo(string[] input)
+        {
+            var passports = ExtractPassports(input);
+
+            return passports.Count(p => PassportContainsAllRequiredFields(p) && PassportContainsValidValuesForEachField(p));
+        }
+
+        public static List<string> ExtractPassports(string[] input)
+        {
+            var passports = new List<string>
+            {
+                string.Empty
+            };
+
+            var currentInputIndex = 0;
+            var currentPassportIndex = 0;
+
+            while (currentInputIndex < input.Length)
+            {
+                if (input[currentInputIndex] != string.Empty)
+                {
+                    passports[currentPassportIndex] += " " + input[currentInputIndex].Replace(": ", ":");
+                    passports[currentPassportIndex] = passports[currentPassportIndex].TrimStart();
+                }
+                else
+                {
+                    passports.Add("");
+                    currentPassportIndex++;
+                }
+
+                currentInputIndex++;
+            }
+
+            return passports;
+		}
+
+        public static bool PassportContainsAllRequiredFields(string passport)
+        {
+            var passportDictionary = GenerateDictionaryForPassport(passport);
+
+            string[] requiredFields = { "byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid" };
+
+            return requiredFields.All(field => passportDictionary.ContainsKey(field));
+        }
+
+        public static bool PassportContainsValidValuesForEachField(string passport)
+        {
+            var passportDictionary = GenerateDictionaryForPassport(passport);
+            
+            if (!YearValueIsValid("byr", 1920, 2002, passportDictionary)) return false;
+
+            if (!YearValueIsValid("iyr", 2010, 2020, passportDictionary)) return false;
+
+            if (!YearValueIsValid("eyr", 2020, 2030, passportDictionary)) return false;
+
+            //var iyrValue = passportDictionary["iyr"];
+            //var iyrInt = Int16.Parse(iyrValue);
+            //if (!(iyrValue.Length == 4 && iyrInt >= 2010 && iyrInt <= 2020))
+            //{
+            //    return false;
+            //}
+
+            //var byrValue = passportDictionary["byr"];
+            //var byrInt = Int16.Parse(byrValue);
+            //if (!(byrValue.Length == 4 && byrInt >= 2020 && byrInt <= 2030))
+            //{
+            //    return false;
+            //}
+
+            var hgtValue = passportDictionary["hgt"];
+            if (hgtValue.EndsWith("cm"))
+            {
+                var hgtInt = short.Parse(hgtValue.Replace("cm", ""));
+                if (!(hgtInt >= 150 && hgtInt <= 193)) return false;
+            }
+            else if (hgtValue.EndsWith("in"))
+            {
+                var hgtInt = short.Parse(hgtValue.Replace("in", ""));
+                if (!(hgtInt >= 59 && hgtInt <= 76)) return false;
+            }
+            else
+            {
+                return false;
+            }
+
+            var hclValue = passportDictionary["hcl"];
+            if (!Regex.IsMatch(hclValue, @"^#[0-9a-f]{6}$")) return false;
+
+            var eclValue = passportDictionary["ecl"];
+            if (!Regex.IsMatch(eclValue, @"(amb|blu|brn|gry|grn|hzl|oth)")) return false;
+
+            var pidValue = passportDictionary["pid"];
+            return Regex.IsMatch(pidValue, @"^\d{9}$");
+        }
+
+        private static bool YearValueIsValid(string propertyName, int min, int max, Dictionary<string, string> passportDictionary)
+        {
+            var propertyValue = passportDictionary[propertyName];
+            var propertyInt = short.Parse(propertyValue);
+            return propertyValue.Length == 4 && propertyInt >= min && propertyInt <= max;
+        }
+
+        private static Dictionary<string,string> GenerateDictionaryForPassport(string passport)
+        {
+            var keyValuePairs = passport.Split();
+
+            return keyValuePairs
+                .Select(keyValuePair => keyValuePair.Split(':'))
+                .ToDictionary(stringParts => stringParts[0], stringParts => stringParts[1]);
+        }
+    }
+}
