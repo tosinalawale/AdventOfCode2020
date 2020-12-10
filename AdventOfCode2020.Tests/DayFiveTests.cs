64e338ffa6c438f7568373cf41e70db0260b75d1
tosinalawale <talawale@hotmail.com>
2020-12-10 08:58:11 +0000

    Day 5 solution - Part 1

---
+++ AdventOfCode2020.Tests/DayFiveTests.cs
@@ -0,0 +1,29 @@
+﻿namespace AdventOfCode2020.Tests
+{
+    using System.IO;
+    using FluentAssertions;
+    using NUnit.Framework;
+
+
+    public class DayFiveTests
+    {
+        [Test]
+        public void Part1_CanFindSeatRowForBoardingPass()
+        {
+            DayFive.FindRow("FBFBBFFRLR").Should().Be(44);
+        }
+
+        [Test]
+        public void Part1_CanFindSeatColumnForBoardingPass()
+        {
+            DayFive.FindColumn("FBFBBFFRLR").Should().Be(5);
+        }
+
+        [Test]
+        public void Part1_CalculateResult()
+        {
+            var input = File.ReadAllLines(@"Input/input5.txt");
+            System.Console.WriteLine(DayFive.CalculateResultForPartOne(input));
+        }
+    }
+}
---
+++ AdventOfCode2020/DayFive.cs
@@ -0,0 +1,58 @@
+﻿namespace AdventOfCode2020
+{
+    using System;
+
+    public class DayFive
+    {
+        public static double FindRow(string boardingPass)
+        {
+            double startingRow = 0;
+            double endingRow = 127;
+            for (var i = 0; i < 7; i++)
+            {
+                double middleRow = (endingRow - startingRow) / 2 + startingRow;
+                if (boardingPass[i].Equals('F'))
+                {
+                    endingRow = (int)Math.Floor(middleRow);
+                }
+                else
+                {
+                    startingRow = (int) Math.Ceiling(middleRow);
+                }
+            }
+
+            return startingRow;
+        }
+
+        public static double FindColumn(string boardingPass)
+        {
+            double startingRow = 0;
+            double endingRow = 8;
+            for (var i = 7; i < boardingPass.Length; i++)
+            {
+                double middleRow = (endingRow - startingRow) / 2 + startingRow;
+                if (boardingPass[i].Equals('L'))
+                {
+                    endingRow = (int)Math.Floor(middleRow);
+                }
+                else
+                {
+                    startingRow = (int)Math.Ceiling(middleRow);
+                }
+            }
+
+            return startingRow;
+        }
+
+        public static double CalculateResultForPartOne(string[] input)
+        {
+            double highestSeatId = 0;
+            foreach (var boardingPass in input)
+            {
+                highestSeatId = Math.Max(FindRow(boardingPass) * 8 + FindColumn(boardingPass), highestSeatId);
+            }
+
+            return highestSeatId;
+        }
+    }
+}
