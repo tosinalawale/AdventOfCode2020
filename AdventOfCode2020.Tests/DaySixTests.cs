2cbe1304943a566b322aaba2ee0bfae86b1b7f23
tosinalawale <talawale@hotmail.com>
2020-12-11 20:21:17 +0000

    Day 6 solution

---
+++ AdventOfCode2020.Tests/DaySixTests.cs
@@ -0,0 +1,74 @@
+﻿namespace AdventOfCode2020.Tests
+{
+    using System.IO;
+    using FluentAssertions;
+    using NUnit.Framework;
+
+
+    public class DaySixTests
+    {
+        [Test]
+        public void Part1_CalculateResultForSimpleExample()
+        {
+            var input = new[]
+            {
+                "abc",
+                "",
+                "a",
+                "b",
+                "c",
+                "",
+                "ab",
+                "ac",
+                "",
+                "a",
+                "a",
+                "a",
+                "a",
+                "",
+                "b"
+            };
+
+            DaySix.CalculateResultForPartOne(input).Should().Be(11);
+        }
+
+        [Test]
+        public void Part1_CalculateResult()
+        {
+            var input = File.ReadAllLines(@"Input/input6.txt");
+            System.Console.WriteLine(DaySix.CalculateResultForPartOne(input));
+        }
+
+        [Test]
+        public void Part2_CalculateResultForSimpleExample()
+        {
+            var input = new[]
+            {
+                "abc",
+                "",
+                "a",
+                "b",
+                "c",
+                "",
+                "ab",
+                "ac",
+                "",
+                "a",
+                "a",
+                "a",
+                "a",
+                "",
+                "b"
+            };
+
+            DaySix.CalculateResultForPartTwo(input).Should().Be(6);
+        }
+
+        [Test]
+        public void Part2_CalculateResult()
+        {
+            var input = File.ReadAllLines(@"Input/input6.txt");
+            System.Console.WriteLine(DaySix.CalculateResultForPartTwo(input));
+        }
+    }
+}
---
+++ AdventOfCode2020/DaySix.cs
@@ -0,0 +1,51 @@
+﻿namespace AdventOfCode2020
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class DaySix
+    {
+        public static int CalculateResultForPartOne(string[] input)
+        {
+            var groups = new List<string>();
+            var currentInputIndex = 0;
+
+            while (currentInputIndex < input.Length)
+            {
+                groups.Add("");
+
+                do
+                {
+                    groups[groups.Count - 1] += input[currentInputIndex];
+                    currentInputIndex++;
+                } while (currentInputIndex < input.Length && input[currentInputIndex] != string.Empty);
+
+                currentInputIndex++;
+            }
+
+            return groups.Sum(grp => new HashSet<char>(grp).Count);
+        }
+
+        public static int CalculateResultForPartTwo(string[] input)
+        {
+            var groups = new List<List<string>>();
+            var currentInputIndex = 0;
+
+            while (currentInputIndex < input.Length)
+            {
+                groups.Add(new List<string>());
+
+                do
+                {
+                    groups[groups.Count - 1].Add(input[currentInputIndex]);
+                    currentInputIndex++;
+                } while (currentInputIndex < input.Length && input[currentInputIndex] != string.Empty);
+
+                currentInputIndex++;
+            }
+
+            return groups.Sum(grp => 
+                grp.Aggregate((p, q) => string.Concat(p.Intersect(q))).Length);
+        }
+    }
+}
