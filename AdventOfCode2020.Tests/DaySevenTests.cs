8c015bd50088dbb51100064a9e3694f07eba650e
tosinalawale <talawale@hotmail.com>
2020-12-13 19:32:18 +0000

    Day 7 solution - part 2

--- AdventOfCode2020.Tests/DaySevenTests.cs
+++ AdventOfCode2020.Tests/DaySevenTests.cs
@@ -32,5 +32,31 @@
             var input = File.ReadAllLines(@"Input/input7.txt");
             System.Console.WriteLine(DaySeven.CalculateResultForPartOne(input));
         }
+
+        [Test]
+        public void Part2_CalculateResultForSimpleExample()
+        {
+            var input = new[]
+            {
+                "light red bags contain 1 bright white bag, 2 muted yellow bags.",
+                "dark orange bags contain 3 bright white bags, 4 muted yellow bags.",
+                "bright white bags contain 1 shiny gold bag.",
+                "muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.",
+                "shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.",
+                "dark olive bags contain 3 faded blue bags, 4 dotted black bags.",
+                "vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.",
+                "faded blue bags contain no other bags.",
+                "dotted black bags contain no other bags."
+            };
+
+            DaySeven.CalculateResultForPartTwo(input).Should().Be(32);
+        }
+
+        [Test]
+        public void Part2_CalculateResult()
+        {
+            var input = File.ReadAllLines(@"Input/input7.txt");
+            System.Console.WriteLine(DaySeven.CalculateResultForPartTwo(input));
+        }
     }
 }
--- AdventOfCode2020/DaySeven.cs
+++ AdventOfCode2020/DaySeven.cs
@@ -14,14 +14,29 @@
             {
                 var inputParts = rule.Split(" bags contain ");
                 var ruleKey = inputParts[0];
-                var ruleValue = ExtractContents(inputParts[1]);
+                var ruleValue = ExtractBagColoursFromContents(inputParts[1]);
                 rules.Add(ruleKey, ruleValue);
             }
 
             return rules.Keys.Count(c => EventuallyContainsBagColour(c, "shiny gold", rules));
         }
+        
+        public static int CalculateResultForPartTwo(string[] input)
+        {
+            var rules = new Dictionary<string, List<string>>();
 
-        private static List<string> ExtractContents(string bagContents)
+            foreach (var rule in input)
+            {
+                var inputParts = rule.Split(" bags contain ");
+                var ruleKey = inputParts[0];
+                var ruleValue = ExtractBagsFromContents(inputParts[1]);
+                rules.Add(ruleKey, ruleValue);
+            }
+
+            return NumberOfBagsRequiredInside("shiny gold", rules);
+        }
+
+        private static List<string> ExtractBagColoursFromContents(string bagContents)
         {
             bagContents = Regex.Replace(bagContents, @"\s*bag(s*),\s*", "|");
             bagContents = Regex.Replace(bagContents, @"\s*bag(s*)\.\s*", string.Empty);
@@ -40,6 +55,30 @@
 
             return bagContents.Contains(bagColour) 
                    || bagContents.Any(b => EventuallyContainsBagColour(b, bagColour, rules));
+        }
+
+        private static List<string> ExtractBagsFromContents(string bagContents)
+        {
+            bagContents = Regex.Replace(bagContents, @"\s*bag(s*),\s*", "|");
+            bagContents = Regex.Replace(bagContents, @"\s*bag(s*)\.\s*", string.Empty);
+            return bagContents.Split('|').ToList();
+        }
+
+        private static int NumberOfBagsRequiredInside(string containerBagColour, Dictionary<string, List<string>> rules)
+        {
+            var bagContents = rules[containerBagColour];
+
+            if (bagContents[0].Equals("no other"))
+            {
+                return 0;
+            }
+
+            return 
+                (from bag in bagContents 
+                let numberOfBags = short.Parse(bag.Split(" ")[0]) 
+                let bagColour = Regex.Replace(bag, @"\s*\d+\s*", string.Empty) 
+                select numberOfBags + numberOfBags * NumberOfBagsRequiredInside(bagColour, rules))
+                .Sum();
         }
     }
 }
