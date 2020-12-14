91b0de2624f6cc8bdeccb819c722d7ae78a38351
tosinalawale <talawale@hotmail.com>
2020-12-14 21:20:38 +0000

    Day 8 solution - part 1

---
+++ AdventOfCode2020.Tests/DayEightTests.cs
@@ -0,0 +1,36 @@
+﻿namespace AdventOfCode2020.Tests
+{
+    using System.IO;
+    using FluentAssertions;
+    using NUnit.Framework;
+
+
+    public class DayEightTests
+    {
+        [Test]
+        public void Part1_CalculateResultForSimpleExample()
+        {
+            var input = new[]
+            {
+                "nop +0",
+                "acc +1",
+                "jmp +4",
+                "acc +3",
+                "jmp -3",
+                "acc -99",
+                "acc +1",
+                "jmp -4",
+                "acc +6"
+            };
+
+            DayEight.CalculateResultForPartOne(input).Should().Be(5);
+        }
+
+        [Test]
+        public void Part1_CalculateResult()
+        {
+            var input = File.ReadAllLines(@"Input/input8.txt");
+            System.Console.WriteLine(DayEight.CalculateResultForPartOne(input));
+        }
+    }
+}
---
+++ AdventOfCode2020/DayEight.cs
@@ -0,0 +1,47 @@
+﻿namespace AdventOfCode2020
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text.RegularExpressions;
+
+    public class DayEight
+    {
+        public static int CalculateResultForPartOne(string[] input)
+        {
+            var accumulator = 0;
+            var visited = new int[input.Length];
+            var pointer = 0;
+            var orderOfVisit = 0;
+
+            while (pointer <= input.Length)
+            {
+                if (visited[pointer] == 0)
+                {
+                    visited[pointer] = ++orderOfVisit;
+                }
+                else
+                {
+                    return accumulator;
+                }
+
+                var instructionParts = input[pointer].Split(" ");
+                var operation = instructionParts[0];
+                var argument = short.Parse(instructionParts[1]);
+
+                if (operation.Equals("acc"))
+                {
+                    accumulator += argument;
+                }
+                else if (operation.Equals("jmp"))
+                {
+                    pointer += argument;
+                    continue;
+                }
+
+                pointer++;
+            }
+
+            return 0;
+        }
+    }
+}
