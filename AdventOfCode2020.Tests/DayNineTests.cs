a63352515258fee6c387d7c49f2776e42ad801e2
tosinalawale <talawale@hotmail.com>
2020-12-16 19:02:09 +0000

    Day 9 solution

---
+++ AdventOfCode2020.Tests/DayNineTests.cs
@@ -0,0 +1,85 @@
+﻿namespace AdventOfCode2020.Tests
+{
+    using System.Collections.Generic;
+    using System.IO;
+    using FluentAssertions;
+    using NUnit.Framework;
+
+
+    public class DayNineTests
+    {
+        [Test]
+        public void Part1_CalculateResultForSimpleExample()
+        {
+            var input = new List<long>()
+            {
+                35,
+                20,
+                15,
+                25,
+                47,
+                40,
+                62,
+                55,
+                65,
+                95,
+                102,
+                117,
+                150,
+                182,
+                127,
+                219,
+                299,
+                277,
+                309,
+                576
+            };
+
+            DayNine.FindNonXmasNumber(input, 5).Should().Be(127);
+        }
+
+        [Test]
+        public void Part1_CalculateResult()
+        {
+            var input = File.ReadAllLines(@"Input/input9.txt");
+            System.Console.WriteLine(DayNine.CalculateResultForPartOne(input));
+        }
+
+        [Test]
+        public void Part2_FindContiguousRangeSummingUpToTotal()
+        {
+            var input = new List<long>()
+            {
+                35,
+                20,
+                15,
+                25,
+                47,
+                40,
+                62,
+                55,
+                65,
+                95,
+                102,
+                117,
+                150,
+                182,
+                127,
+                219,
+                299,
+                277,
+                309,
+                576
+            };
+
+            DayNine.FindContiguousRangeSummingUpToTotal(127, input).Should().Be((2, 5));
+        }
+
+        [Test]
+        public void Part2_CalculateResult()
+        {
+            var input = File.ReadAllLines(@"Input/input9.txt");
+            System.Console.WriteLine(DayNine.CalculateResultForPartTwo(input));
+        }
+    }
+}
---
+++ AdventOfCode2020/DayNine.cs
@@ -0,0 +1,95 @@
+﻿namespace AdventOfCode2020
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class DayNine
+    {
+        public static long CalculateResultForPartOne(string[] input)
+        {
+            var inputNumbers = input.Select(long.Parse).ToList();
+
+            return FindNonXmasNumber(inputNumbers, 27);
+        }
+
+        public static long FindNonXmasNumber(List<long> inputNumbers, int preambleLength)
+        {
+            for (var i = preambleLength; i < inputNumbers.Count; i++)
+            {
+                if (!IsMatch(inputNumbers, i, preambleLength))
+                {
+                    return inputNumbers[i];
+                }
+            }
+
+            return int.MinValue;
+        }
+
+        public static (int, int) FindContiguousRangeSummingUpToTotal(long targetTotal, List<long> input)
+        {
+            for (var startIndex = 0; startIndex < input.Count - 1; startIndex++)
+            {
+                var endIndex = TestRangesFrom(startIndex, input, targetTotal);
+                if (endIndex != -1)
+                {
+                    return (startIndex, endIndex);
+                }
+            }
+
+            return (0, -1);
+        }
+
+        public static long CalculateResultForPartTwo(string[] input)
+        {
+            var inputNumbers = input.Select(long.Parse).ToList();
+
+            var nonXmasNumber = FindNonXmasNumber(inputNumbers, 27);
+
+            var (startIndex, endIndex) = FindContiguousRangeSummingUpToTotal(nonXmasNumber, inputNumbers);
+
+            var min = long.MaxValue;
+            var max = long.MinValue;
+
+            for (var i = startIndex; i <= endIndex; i++)
+            {
+                var number = inputNumbers[i];
+                min = Math.Min(min, number);
+                max = Math.Max(max, number);
+            }
+
+            return min + max;
+        }
+
+        private static int TestRangesFrom(int startIndex, List<long> input, long targetSum)
+        {
+            long sum = 0;
+            for (var endIndex = startIndex; endIndex < input.Count; endIndex++)
+            {
+                sum += input[endIndex];
+                if (sum == targetSum && endIndex != startIndex)
+                {
+                    return endIndex;
+                }
+            }
+
+            return -1;
+        }
+
+        private static bool IsMatch(List<long> input, int index, int preambleLength)
+        {
+            for (var x = index - preambleLength; x < index; x++)
+            {
+                for (var y = x + 1; y < index; y++)
+                {
+                    if (input[x] + input[y] == input[index])
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+    }
+}
