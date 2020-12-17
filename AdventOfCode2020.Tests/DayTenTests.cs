848c06ae49a6c6fd1ad0db06a608035c20f9cc70
tosinalawale <talawale@hotmail.com>
2020-12-17 19:18:04 +0000

    Day 10 solution - part 1

---
+++ AdventOfCode2020.Tests/DayTenTests.cs
@@ -0,0 +1,80 @@
+﻿namespace AdventOfCode2020.Tests
+{
+    using System.Collections.Generic;
+    using System.IO;
+    using FluentAssertions;
+    using NUnit.Framework;
+
+
+    public class DayTenTests
+    {
+        [Test]
+        public void Part1_GetJoltageDistributionForSimpleExample()
+        {
+            var input = new List<int>()
+            {
+                16,
+                10,
+                15,
+                5,
+                1,
+                11,
+                7,
+                19,
+                6,
+                12,
+                4
+            };
+
+            DayTen.GetJoltageDistrubutionForAdapterChain(input).Should().Be((7, 0, 5));
+        }
+
+        [Test]
+        public void Part1_GetJoltageDistributionForLargerExample()
+        {
+            var input = new List<int>()
+            {
+                28,
+                33,
+                18,
+                42,
+                31,
+                14,
+                46,
+                20,
+                48,
+                47,
+                24,
+                23,
+                49,
+                45,
+                19,
+                38,
+                39,
+                11,
+                1,
+                32,
+                25,
+                35,
+                8,
+                17,
+                7,
+                9,
+                4,
+                2,
+                34,
+                10,
+                3
+            };
+
+            DayTen.GetJoltageDistrubutionForAdapterChain(input).Should().Be((22, 0, 10));
+        }
+
+        [Test]
+        public void Part1_CalculateResult()
+        {
+            var input = File.ReadAllLines(@"Input/input10.txt");
+            System.Console.WriteLine(DayTen.CalculateResultForPartOne(input));
+        }
+    }
+}
---
+++ AdventOfCode2020/DayTen.cs
@@ -0,0 +1,38 @@
+﻿namespace AdventOfCode2020
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class DayTen
+    {
+        public static int CalculateResultForPartOne(string[] input)
+        {
+            var adapterRatings = input.Select(int.Parse).ToList();
+
+            var (ones, twos, threes) = GetJoltageDistrubutionForAdapterChain(adapterRatings);
+
+            return ones * threes;
+        }
+
+        public static (int ones, int twos, int threes) GetJoltageDistrubutionForAdapterChain(List<int> adapterRatings)
+        {
+            adapterRatings.Sort();
+
+            var adapterChainJoltageDifferences = new int[adapterRatings.Count + 1];
+
+            adapterChainJoltageDifferences[0] = adapterRatings[0];
+            adapterChainJoltageDifferences[^1] = 3;
+
+            for (var i = 1; i < adapterRatings.Count; i++)
+            {
+                adapterChainJoltageDifferences[i] = adapterRatings[i] - adapterRatings[i-1];
+            }
+
+            var ones = adapterChainJoltageDifferences.Count(x => x == 1);
+            var twos = adapterChainJoltageDifferences.Count(x => x == 2);
+            var threes = adapterChainJoltageDifferences.Count(x => x == 3);
+
+            return (ones, twos, threes);
+        }
+    }
+}
